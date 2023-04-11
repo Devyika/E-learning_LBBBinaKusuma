807d113b5ca3c09dd7301d2efac352ad7af1a4f6
Moch. Nadi Rafli Maulana <mochammadnadiraflimaulana@gmail.com>
2023-04-11 11:40:17 +0700

    Admin Dashboard - MochNad

---
+++ resources/views/admin/layout/navbar.blade.php
@@ -0,0 +1,39 @@
+<nav class="main-header navbar navbar-expand navbar-light bg-primary">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link bg-primary" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+        </li>
+        <li class="nav-item d-none d-sm-inline-block">
+            <a href="{{ url('/dashboard') }}" class="nav-link bg-primary">Dashboard</a>
+        </li>
+        <li class="nav-item d-none d-sm-inline-block">
+            <a href="{{ url('/user') }}" class="nav-link bg-primary">User</a>
+        </li>
+        <li class="nav-item d-none d-sm-inline-block">
+            <a href="{{ url('/dashboard') }}" class="nav-link bg-primary">Mata Pelajaran</a>
+        </li>
+    </ul>
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+        <!-- Messages Dropdown Menu -->
+        <li class="nav-item dropdown">
+            
+        <!-- Notifications Dropdown Menu -->
+        <li class="nav-item">
+            <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+            <i class="fas fa-expand-arrows-alt"></i>
+            </a>
+        </li>
+        <li class="nav-item user-panel">
+            <a data-widget="control-sidebar" data-controlsidebar-slide="true" href="#" role="button">
+            <div class="mt-1 d-flex">
+                <div class="image">
+                <img src="{{ asset('assets/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
+                </div>
+            </div>
+            </a>
+        </li>
+    </ul>
+  </nav>
