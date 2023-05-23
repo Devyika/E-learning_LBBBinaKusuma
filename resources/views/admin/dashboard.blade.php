7cee92ad680c8d5a93c60dfce591c9c16265822d
Rizaldy Ali Machfuddin <alirizaldy357@gmail.com>
2023-05-23 08:21:50 +0700

    edit dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -127,7 +127,7 @@
     </div>
 </div>
 <div class="row">
-    <div class="col-xl-6 col-md-6 mb-4">
+    <div class="col-xl-12 col-md-6 mb-4">
         <div class="card border-left-info shadow h-100 py-2">
             <div class="card-body">
                 <div class="row no-gutters align-items-center">
@@ -138,22 +138,6 @@
                     </div>
                     <div class="col-auto">
                         <i class="fas fa-swatchbook fa-2x"></i>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-xl-6 col-md-6 mb-4">
-        <div class="card border-left-info shadow h-100 py-2">
-            <div class="card-body">
-                <div class="row no-gutters align-items-center">
-                    <div class="col mr-2">
-                        <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
-                            Pertemuan</div>
-                        <div class="h5 mb-0 font-weight-bold">{{ $countPertemuan }} Pertemuan</div>
-                    </div>
-                    <div class="col-auto">
-                        <i class="fas fa-comment-alt fa-2x"></i>
                     </div>
                 </div>
             </div>
